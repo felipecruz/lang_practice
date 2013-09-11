4c101c460d0053bc597f474b82d5b5eeaf12c7f4
Felipe cruz <felipecruz@loogica.net>
2013-09-11 18:13:11 -0300

    Run monger parser in all spec files and check RC
    for each file in specs/ this script will execute
    monks $filname and check it's return code to
    display a message

--- monga/run_specs.sh
+++ monga/run_specs.sh
@@ -1,9 +1,14 @@
-#!/bin/bash
+#!/bin/sh
 
-FILES=specs/*
-
-for f in $FILES
+find specs -name "*.monga" |
+while read filename
 do
-  echo "Processing $f file..."
-  ./monga < $f
+    echo "Evaluating $filename: \c"
+    ./monga "$filename" > /dev/null 2>&1
+    OUT=$?
+    if [ $OUT -eq 0 ]; then
+        echo "... Valid File"
+    else
+        echo "... Invalid File"
+    fi
 done
