cb73359c4c967bf1b9ccb6081cad53938d9752af
Felipe cruz <felipecruz@loogica.net>
2013-09-11 22:46:23 -0300

    Run specs and check its output with expected files

--- monga/run_specs.sh
+++ monga/run_specs.sh
@@ -4,11 +4,26 @@ find specs -name "*.monga" |
 while read filename
 do
     echo "Evaluating $filename: \c"
-    ./monga "$filename" > /dev/null 2>&1
+    ./monga "$filename" > "temp_output.out" 2> /dev/null
     OUT=$?
+
     if [ $OUT -eq 0 ]; then
         echo "... Valid File"
     else
         echo "... Invalid File"
     fi
+
+    CHECK_FILE=${filename/monga/expected}
+
+    diff "temp_output.out" $CHECK_FILE
+    OUT=$?
+
+    if [ $OUT -eq 0 ]; then
+        echo "... Tests OK"
+    else
+        echo "... Test Error"
+    fi
+
+    echo ""
+    rm temp_output.out
 done
