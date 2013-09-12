9f2da3ecbfd9cec008400a14764d935187395324
Felipe cruz <felipecruz@loogica.net>
2013-09-11 22:46:08 -0300

    add script that generates tests outputs

---
+++ monga/generate_outputs.sh
@@ -0,0 +1,6 @@
+./monga specs/condicionais.spec.monga > specs/condicionais.spec.expected
+./monga specs/hexa.spec.monga > specs/hexa.spec.expected
+./monga specs/main.spec.monga > specs/main.spec.expected
+./monga specs/errors/error_invalid_integer.monga > specs/errors/error_invalid_integer.expected
+./monga specs/errors/error_invalid_hexa.monga > specs/errors/error_invalid_hexa.expected
+./monga specs/errors/error_invalid_float.monga > specs/errors/error_invalid_float.expected
