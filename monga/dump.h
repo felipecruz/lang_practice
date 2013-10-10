8cff4fb4ec6d4d4e90409b53e2027497a9da6956
Felipe cruz <felipecruz@loogica.net>
2013-10-09 23:38:35 -0300

    add dump.h

---
+++ monga/dump.h
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "astnodes.h"
+
+#ifndef _DUMP_H_
+#define _DUMP_H_
+void error (const char *message);
+void dump_Program (Program *program, int level);
+void print_Decl (Decl *decl, int level);
+void print_NameList (NameList *name_list, int level);
+void print_Params (Params *_params, int level);
+void print_Call (Call *call, int level);
+void print_Exp (Exp *exp, int level);
+void print_Cmd (Cmd *cmd, int level);
+void print_Var (struct Var *var, int level);
+void print_Type (Type *type, int level);
+void print_Block (Block *block, int level);
+#endif
