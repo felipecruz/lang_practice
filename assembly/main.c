9bc4e690dc38499173580a4e5289a28aab64fb2e
Felipe cruz <felipecruz@loogica.net>
2013-12-01 17:01:04 -0200

    Examples to stuy

--- assembly/main.c
+++ assembly/main.c
@@ -1,4 +1,8 @@
+void pint (int x) {
+    printf ("%d\n", x);
+}
 
 int main (int argc) {
+    pint (1);
     return 0;
 }
--- assembly/main.s
+++ assembly/main.s
@@ -1,9 +1,31 @@
+	.data
+LC0:
+	.ascii "%d\12\0"
 	.text
+.globl _pint
+_pint:
+	pushl	%ebp
+	movl	%esp, %ebp
+	pushl	%ebx
+	
+    subl	$20, %esp
+
+	movl	8(%ebp), %eax
+	movl	%eax, 4(%esp)
+	leal	LC0, %eax
+	movl	%eax, (%esp)
+	call	_printf
+	addl	$8, %esp
+	popl	%ebx
+	leave
+	ret
 .globl _main
 _main:
 	pushl	%ebp
 	movl	%esp, %ebp
-	subl	$8, %esp
+	subl	$24, %esp
+	movl	$2, (%esp)
+	call	_pint
 	movl	$0, %eax
 	leave
 	ret
